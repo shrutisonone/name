b250fb35d47b6f07589123c0b7d38a8ee7871eb8
Rahul Sonone <rahulsonone1234@gmail.com>
2020-11-22 13:25:48 +0530

    Update day1) array.cpp

--- day1) array.cpp
+++ day1) array.cpp
@@ -1,4 +1,4 @@
-cate//Find the duplicate in an array of N+1 integers. 
+//Find the duplicate in an array of N+1 integers. 
 1) sort array and then check for i and i+1 element if same then that would be duplicate element
 2) create frequency array and then check frequency
 3) slow and fast pointer and then check cycle
@@ -21,3 +21,29 @@ int findDuplicate(vector<int>& nums)
         
         return s;
 }
+//Sort an array of 0’s 1’s 2’s without using extra space or sorting algo 
+1) sort(a, a+n);
+2) count number of 0's , 1's , & 2's and then print 0's, 1's & 2's
+3) Dutch National Flag using low , mid & high
+
+void sortColors(vector<int>& nums) {
+        int low=0;
+        int high=nums.size()-1;
+        int mid=0;
+        
+        while(mid<=high){
+            switch(nums[mid]){
+                    
+                case 0:
+                        swap(nums[low++], nums[mid++]);
+                        break;
+                case 1:
+                        mid++;
+                        break;
+                case 2: 
+                        swap(nums[mid], nums[high--]);
+                        break;
+            }
+        }
+        
+    }
