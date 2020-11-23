546cde6c95529a4211afa4ceff0820c6a20814d0
Rahul Sonone <rahulsonone1234@gmail.com>
2020-11-23 15:44:02 +0530

    Update day1) array.cpp

--- day1) array.cpp
+++ day1) array.cpp
@@ -47,3 +47,28 @@ void sortColors(vector<int>& nums) {
         }
         
     }
+//Repeating and Missing Number
+1) sort the array and then check difference between consecutive number if 1 then continue if 0 then repeating and then missing one calculate sum and then check
+2) sum of n numbers n(n+1)/2 and sum of the squares n(n+1)(2n+1)/6
+   and then X-Y=certain value (where certain value is difference between sum of given array and the real array that needs to be) 
+   and then X-Y=certain value (---||---)(just change it two squares)
+   and then solve these above two Equations
+3) Using XOR
+   XOR the given array and (4, 3, 6, 2, 1, 1)
+   4^3^6^2^1^1=3
+   3^(1^2^3^4^5^6)=4
+   X^Y=4
+   1 0 0
+   so either of the bits would be 1/0 or 1/0
+   so second bit 
+   then XOR with all required
+   then create two buckets and then if bits are set keep it in one set and if the bits are not set put in another
+   |    6  |       |     3  |
+   |    5  |       |     2  |
+   |    4  |       |     1  | 
+   |    6  |       |     1  |
+   |___ 4__|       |     1  |
+                   |     2  |
+        5          |____ 3__|
+                          1
+           
