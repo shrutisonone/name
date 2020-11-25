aefca2fb0e2be2104824dd7659c7239f21105fcc
Rahul Sonone <rahulsonone1234@gmail.com>
2020-11-25 08:48:37 +0530

    Update day1) array.cpp

--- day1) array.cpp
+++ day1) array.cpp
@@ -136,3 +136,49 @@ void getTwoElements(int arr[], int n,
 	/* *x and *y hold the desired
 		output elements */
 }
+//Merge two sorted Arrays without extra space 
+1) Take another array which is of the size of the sum of the other two arrays put all the elements in the other array and then sort that array
+2) If we aren't using  any extra space then we can use the technique of insertion sort an then play with two array check if smaller then ok else swap
+1 4 7 8 10
+2 3 9
+
+1 2 7 8 10  --->sort    1 2 7 8 10
+4 3 9                   3 4 9
+
+1 2 3 8 10  ---->sort   1 2 3 8 10
+7 4 9                   4 7 9
+
+	// Function to find next gap.
+int nextGap(int gap)
+{
+    if (gap <= 1)
+        return 0;
+    return (gap / 2) + (gap % 2);
+}
+ 
+void merge(int* arr1, int* arr2, int n, int m)
+{
+    int i, j, gap = n + m;
+    for (gap = nextGap(gap); 
+         gap > 0; gap = nextGap(gap)) 
+    {
+        // comparing elements in the first array.
+        for (i = 0; i + gap < n; i++)
+            if (arr1[i] > arr1[i + gap])
+                swap(arr1[i], arr1[i + gap]);
+ 
+        // comparing elements in both arrays.
+        for (j = gap > n ? gap - n : 0; 
+             i < n && j < m;
+             i++, j++)
+            if (arr1[i] > arr2[j])
+                swap(arr1[i], arr2[j]);
+ 
+        if (j < m) {
+            // comparing elements in the second array.
+            for (j = 0; j + gap < m; j++)
+                if (arr2[j] > arr2[j + gap])
+                    swap(arr2[j], arr2[j + gap]);
+        }
+    }
+}
