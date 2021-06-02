1aaec4d05a4c77ece81ea0b0619cbf8826973e82
Rahul Sonone <rahulsonone1234@gmail.com>
2021-06-02 21:17:28 +0530

    Update day1) array.cpp

--- day1) array.cpp
+++ day1) array.cpp
@@ -1548,7 +1548,31 @@ class Solution
 };
 
 	
-
+//Min Number of Coins Greedy
+	
+int findmincoins(int V)
+{
+    int deno={1, 2, 5, 10, 20, 50, 100, 500, 1000};
+    int n=9;
+    
+    vector<int>ans;
+    
+    for(int i=n-1;i>=0;i--)
+    {
+        while(V>=deno[i]){
+            V-=deno[i];
+            ans.push_back(deno[i]);
+        }
+    }
+    
+    for(int i=0;i<ans.size();i++)
+    {
+        cout<<ans[i]<<" ";
+    }
+    cout<<"Min Number of Coins"<<ans.size()<<endl;
+}
+	
+	
 
 
 
