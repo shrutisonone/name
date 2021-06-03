6f756d82ca04aa44a327fc83baf22993a6ff0616
Rahul Sonone <rahulsonone1234@gmail.com>
2021-06-03 13:48:47 +0530

    Update day1) array.cpp

--- day1) array.cpp
+++ day1) array.cpp
@@ -1604,6 +1604,32 @@ int findmincoins(int V)
         return SubsetSum;
     }
 	
-
+//subset sum 2 where find power set without duplicates
+	
+class Solution {
+public:
+    void func(int ind, vector<int>&nums, vector<int>&ds, vector<vector<int>>&ans)
+    {
+        ans.push_back(ds);
+        for(int i=ind;i<nums.size();i++)
+        {
+            if(i!=ind && nums[i]==nums[i-1])    continue;
+            ds.push_back(nums[i]);
+            func(i+1, nums, ds, ans);
+            ds.pop_back();
+        }
+        
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+        vector<vector<int>>ans;
+        vector<int>ds;
+        sort(nums.begin(), nums.end());
+        
+        func(0, nums, ds, ans);
+        return ans;
+    }
+};
 
 
