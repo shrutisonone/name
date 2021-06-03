82de450e03d2b6b4a560b762ff51e2fdddd8acba
Rahul Sonone <rahulsonone1234@gmail.com>
2021-06-03 14:36:36 +0530

    Update day1) array.cpp

--- day1) array.cpp
+++ day1) array.cpp
@@ -1632,4 +1632,42 @@ public:
     }
 };
 
+	
+//Combination Sum 1
+// Input: candidates = [2,3,6,7], target = 7
+// Output: [[2,2,3],[7]]
+// Explanation:
+// 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+// 7 is a candidate, and 7 = 7.
+// These are the only two combinations.	
+	class Solution {
+public:
+    void func(int ind, int target, vector<int>&candidates, vector<vector<int>>&ans, vector<int>&ds)
+    {
+        if(ind==candidates.size())
+        {
+            if(target==0)
+            {
+                ans.push_back(ds);
+            }
+            return;
+        }
+        
+        if(candidates[ind]<=target)
+        {
+            ds.push_back(candidates[ind]);
+            func(ind, target-candidates[ind], candidates, ans, ds);
+            ds.pop_back();
+        }
+        func(ind+1, target, candidates, ans, ds);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+        vector<vector<int>>ans;
+        vector<int>ds;
+        
+        func(0, target, candidates, ans, ds);
+        return ans;
+    }
+};
 
