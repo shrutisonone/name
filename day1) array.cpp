05b76615444005f853795646fa4a69992970733d
Rahul Sonone <rahulsonone1234@gmail.com>
2021-06-06 11:22:24 +0530

    Update day1) array.cpp

--- day1) array.cpp
+++ day1) array.cpp
@@ -1872,3 +1872,160 @@ double getNthRoot(int n, int m)
     cout<<low<<" "<<high<<endl;
     cout<<pow(m, (double)(1.0/(double)n));
 }
+//Median in Rowwise and Columnwise sorted array
+	int countSmallerThanMid(vector<int> &row, int mid) {
+    int l = 0, h = row.size() - 1; 
+    while(l <= h) {
+        int md = (l + h) >> 1; 
+        if(row[md] <= mid) {
+            l = md + 1;
+        }
+        else {
+            h = md - 1;
+        }
+    }
+    return l; 
+}
+int Solution::findMedian(vector<vector<int> > &A) {
+    int low = INT_MIN;
+    int high = INT_MAX; 
+    int n = A.size();
+    int m = A[0].size(); 
+    while(low <= high) {
+        int mid = (low + high) >> 1; 
+        int cnt = 0;
+        for(int i = 0;i<n;i++) {
+            cnt += countSmallerThanMid(A[i], mid); 
+        }
+        
+        if(cnt <= (n * m) / 2) low = mid + 1; 
+        else high = mid - 1; 
+    }
+    return low; 
+}
+	
+	
+//Single in array of duplicates
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low=0;
+        int high=nums.size()-2;
+        
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]==nums[mid^1])
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        return nums[low];
+    }
+};
+	
+// Search Element in rotated sorted array
+
+class Solution {
+public:
+    int search(vector<int>& a, int target) {
+        int low = 0, high = a.size() - 1; 
+        while(low <= high) {
+            int mid = (low + high) >> 1; 
+            if(a[mid] == target) return mid; 
+            
+            // the left side is sorted
+            if(a[low] <= a[mid]) {
+                if(target >= a[low] && target <= a[mid]) {
+                    high = mid - 1; 
+                }
+                else {
+                    low = mid + 1; 
+                }
+            }
+            else {
+                if(target >= a[mid] && target <= a[high]) {
+                    low = mid + 1; 
+                }
+                else {
+                    high = mid - 1; 
+                }
+            }
+        } 
+        return -1; 
+    }
+};
+	
+//Median of two sorted arrays
+	
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if(nums2.size() < nums1.size()) return findMedianSortedArrays(nums2, nums1);
+        int n1 = nums1.size();
+        int n2 = nums2.size(); 
+        int low = 0, high = n1;
+        
+        while(low <= high) {
+            int cut1 = (low+high) >> 1;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1; 
+            
+        
+            int left1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
+            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2-1]; 
+            
+            int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+            int right2 = cut2 == n2 ? INT_MAX : nums2[cut2]; 
+            
+            
+            if(left1 <= right2 && left2 <= right1) {
+                if( (n1 + n2) % 2 == 0 ) 
+                    return (max(left1, left2) + min(right1, right2)) / 2.0; 
+                else 
+                    return max(left1, left2); 
+            }
+            else if(left1 > right2) {
+                high = cut1 - 1; 
+            }
+            else {
+                low = cut1 + 1; 
+            }
+        }
+        return 0.0; 
+    }
+};
+	
+//K th largest Element of two sorted array
+int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        if(n > m) {
+            return kthElement(arr2, arr1, m, n, k); 
+        }
+        
+        int low = max(0,k-m), high = min(k,n);
+        
+        while(low <= high) {
+            int cut1 = (low + high) >> 1; 
+            int cut2 = k - cut1; 
+            int l1 = cut1 == 0 ? INT_MIN : arr1[cut1 - 1]; 
+            int l2 = cut2 == 0 ? INT_MIN : arr2[cut2 - 1];
+            int r1 = cut1 == n ? INT_MAX : arr1[cut1]; 
+            int r2 = cut2 == m ? INT_MAX : arr2[cut2]; 
+            
+            if(l1 <= r2 && l2 <= r1) {
+                return max(l1, l2);
+            }
+            else if (l1 > r2) {
+                high = cut1 - 1;
+            }
+            else {
+                low = cut1 + 1; 
+            }
+        }
+        return 1; 
+    }
+    
