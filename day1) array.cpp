5cc9fadd3edf4cf2ed6c28c68b3020f9e0234bf5
Rahul Sonone <rahulsonone1234@gmail.com>
2021-06-19 13:48:09 +0530

    Update day1) array.cpp

--- day1) array.cpp
+++ day1) array.cpp
@@ -1984,16 +1984,20 @@ public:
 };
 	
 //Median of two sorted arrays
-	
+// find partition and then check even and odd size and then log(min(m, n)); 	
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums2.size() < nums1.size()) return findMedianSortedArrays(nums2, nums1);
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+    {
+        if(nums2.size() < nums1.size())
+		return findMedianSortedArrays(nums2, nums1);
+	    
         int n1 = nums1.size();
         int n2 = nums2.size(); 
         int low = 0, high = n1;
         
-        while(low <= high) {
+        while(low <= high)
+	{
             int cut1 = (low+high) >> 1;
             int cut2 = (n1 + n2 + 1) / 2 - cut1; 
             
@@ -2005,16 +2009,19 @@ public:
             int right2 = cut2 == n2 ? INT_MAX : nums2[cut2]; 
             
             
-            if(left1 <= right2 && left2 <= right1) {
+            if(left1 <= right2 && left2 <= right1)
+	    {
                 if( (n1 + n2) % 2 == 0 ) 
                     return (max(left1, left2) + min(right1, right2)) / 2.0; 
                 else 
                     return max(left1, left2); 
             }
-            else if(left1 > right2) {
+            else if(left1 > right2)
+	    {
                 high = cut1 - 1; 
             }
-            else {
+            else 
+	    {
                 low = cut1 + 1; 
             }
         }
@@ -2022,30 +2029,39 @@ public:
     }
 };
 	
-//K th largest Element of two sorted array
-int kthElement(int arr1[], int arr2[], int n, int m, int k)
+//K th element in final sorted array
+//log(min(m, n))
+	
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {
-        if(n > m) {
+        if(n > m) 
+	{
             return kthElement(arr2, arr1, m, n, k); 
         }
         
-        int low = max(0,k-m), high = min(k,n);
+        int low = max(0,k-m);
+	int high = min(k,n);
         
-        while(low <= high) {
+        while(low <= high) 
+	{
             int cut1 = (low + high) >> 1; 
             int cut2 = k - cut1; 
+		
             int l1 = cut1 == 0 ? INT_MIN : arr1[cut1 - 1]; 
             int l2 = cut2 == 0 ? INT_MIN : arr2[cut2 - 1];
             int r1 = cut1 == n ? INT_MAX : arr1[cut1]; 
             int r2 = cut2 == m ? INT_MAX : arr2[cut2]; 
             
-            if(l1 <= r2 && l2 <= r1) {
+            if(l1 <= r2 && l2 <= r1) 
+	    {
                 return max(l1, l2);
             }
-            else if (l1 > r2) {
+            else if (l1 > r2) 
+	    {
                 high = cut1 - 1;
             }
-            else {
+            else
+	    {
                 low = cut1 + 1; 
             }
         }
