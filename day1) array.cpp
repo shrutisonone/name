7f166f2e1cd35366b84968fece776000f9c54e40
Rahul Sonone <rahulsonone1234@gmail.com>
2021-07-07 11:06:31 +0530

    Update day1) array.cpp

--- day1) array.cpp
+++ day1) array.cpp
@@ -2192,3 +2192,23 @@ int main()
     }
     return 0;
 }
+
+//Stack Using Array
+
+void MyStack :: push(int x)
+{
+    // Your Code
+    arr[top++]=x;
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{
+    // Your Code 
+    if(top==-1)
+    return -1;
+    
+    return arr[--top];
+}
+	
+	
